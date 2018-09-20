012bd5e4d6395a08bc96b8ea459069d61d41e5fd
Jonathan Rys <jonathan.rk.rys@gmail.com>
2018-09-19 21:32:09 -0400

    Add ProductInfo component

---
+++ ProductInfo/ProductInfo.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+
+export default class ProductInfo extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return <View />;
+  }
+}
+
+const styles = StyleSheet.create({});
